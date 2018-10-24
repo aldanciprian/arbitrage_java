d7cbb089b21008caa30c83b9f8b3516c5ecace7c
aldanciprian <aldanciprian@gmail.com>
2018-10-24 17:02:16 +0300

    connect DB

--- arbitrage/src/main/java/App.java
+++ arbitrage/src/main/java/App.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
+import java.sql.*;
 
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.ExchangeFactory;
@@ -33,6 +34,47 @@ public class App {
 	public static List<PotentialPair> ppair_list =  null;
 	public static Map<String,List<CurrencyPair>> all_eth_symbols = null;
 	
+	public static void connectDB()
+	{
+		 try {
+	         // Step 1: Allocate a database 'Connection' object
+	         Connection conn = DriverManager.getConnection(
+	               "jdbc:mysql://localhost:3306/arbitrage", "ciprian", "ciprian");
+	               // MySQL: "jdbc:mysql://hostname:port/databaseName", "username", "password"
+	 
+	         // Step 2: Allocate a 'Statement' object in the Connection
+	         Statement stmt = conn.createStatement();
+
+	         // Step 3: Execute a SQL SELECT query, the query result
+	         //  is returned in a 'ResultSet' object.
+	         String strSelect = "show tables";
+	         System.out.println("The SQL query is: " + strSelect); // Echo For debugging
+	         System.out.println();
+	         stmt.execute("show tables");
+	         
+	         ResultSet rset = stmt.executeQuery(strSelect);
+//	 
+//	         // Step 4: Process the ResultSet by scrolling the cursor forward via next().
+//	         //  For each row, retrieve the contents of the cells with getXxx(columnName).
+	         System.out.println("The records selected are:"+rset);
+	         int rowCount = 0;
+	         while(rset.next()) {   // Move the cursor to the next row, return false if no more row
+	        	 ResultSetMetaData resm = rset.getMetaData();
+	        	 System.out.println(resm.getTableName(0));
+//	            String title = rset.getString("title");
+//	            double price = rset.getDouble("price");
+//	            int    qty   = rset.getInt("qty");
+//	            System.out.println(title + ", " + price + ", " + qty);
+	            ++rowCount;
+	         }
+//	         System.out.println("Total number of records = " + rowCount);
+			 
+		 
+		      } catch(SQLException ex) {
+		         ex.printStackTrace();
+		      }		
+	}
+	
 	public static void connectExchanges() {
 		for ( String s: exchangesNames)
 		{
@@ -58,6 +100,8 @@ public class App {
 	
 	public static void init()
 	{
+		connectDB();
+		System.exit(0);
 		tradable_pairs = new HashMap<String,List<CurrencyPair>>();
 
 		
